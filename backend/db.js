803c1ef1e899f231c37316e58c1f28c4ae5641ad
Vaidhehi Balaji <yourgithubemail@gmail.com>
2026-04-14 22:17:35 +0530

    Updated DB connection for Railway

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,22 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'bookstore'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-db.connect(err => {
-    if (err) console.log(err);
-    else console.log("DB Connected");
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Connected to Railway MySQL");
+  }
 });
 
 module.exports = db;
